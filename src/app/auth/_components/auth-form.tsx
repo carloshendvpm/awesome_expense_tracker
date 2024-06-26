8ed47d22c7f8197c0fc38cc18ae9614d8b04e0a8
Carlos Henrique <hencarlosdv@gmail.com>
2024-06-25 23:47:33 -0300

    feat: config for auth

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -1,10 +1,19 @@
+'use client'
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { useForm } from "react-hook-form"
+import { signIn } from "next-auth/react"
 
 export function AuthForm() {
+
+  const form = useForm()
+
+  const handleSubmit = form.handleSubmit(async (data) => {
+
+    await signIn('email', { email: data.email })
+  })
   return (
     <main>
       <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 md:px-6">
@@ -14,10 +23,10 @@ export function AuthForm() {
             <CardDescription>Enter your email below and we'll send you a magic link to sign in.</CardDescription>
           </CardHeader>
           <CardContent>
-            <form className="space-y-4">
+            <form onSubmit={handleSubmit} className="space-y-4">
               <div>
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" className="mt-1" />
+                <Input id="email" type="email" placeholder="m@example.com" className="mt-1" {...form.register('email')} />
               </div>
               <Button type="submit" className="w-full">
                 Send Magic Link
