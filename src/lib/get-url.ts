ef3c6a9dcf15faa54dfa4004ec78d797cd800e97
Carlos Henrique <hencarlosdv@gmail.com>
2024-07-02 05:34:32 -0300

    ref: using next public base url

--- src/lib/get-url.ts
+++ src/lib/get-url.ts
@@ -1,5 +1,5 @@
 export function getUrl(path?: string) {
-  const baseUrl = process.env.BASE_URL || ""
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ""
   const normalizedPath = path && path.startsWith("/") ? path : `/${path}`
   return `${baseUrl}${normalizedPath}`
 }
