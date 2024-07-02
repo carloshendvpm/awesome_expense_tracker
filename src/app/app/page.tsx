c0a9a453e3412972f8dc758fdf6b5602980b2638
Carlos Henrique <hencarlosdv@gmail.com>
2024-07-02 05:35:39 -0300

    ref: simple page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,16 @@
-export default function Page() {
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import { auth } from "@/services/auth"
+
+export default async function Page() {
+  const session = await auth()
   return (
-    <h1>page</h1>
+    <main className="h-screen flex items-center justify-center flex-col">
+      <div className="flex items-center gap-4">
+        <Avatar>
+          <AvatarFallback className="capitalize font-bold">{session?.user?.email?.charAt(0)}</AvatarFallback>
+        </Avatar>
+        <span>{session?.user?.email}</span>
+      </div>
+    </main>
   )
 }
